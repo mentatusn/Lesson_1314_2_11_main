e5ea6f709d1f2f3fe22b3b4d79f86f52b9467ac6
Andej Nesterenko <mentatus.n@gmail.com>
2021-08-30 21:35:27 +0300

    Build variants как результат пересечения types и flavors

--- app/src/main/java/ru/geekbrains/lesson_1423_2_11_main/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/lesson_1423_2_11_main/MainActivity.kt
@@ -2,10 +2,12 @@ package ru.geekbrains.lesson_1423_2_11_main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Toast.makeText(this, BuildConfig.PASSWORD,Toast.LENGTH_SHORT).show()
     }
 }
